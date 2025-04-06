486ba3d8ae4f954a7d227d69b04c8c1de03d091b
MaxNoragami <maxcraftmd@gmail.com>
2025-04-06 12:39:44 +0300

    feat: change the commit to the console via local editor

--- Task1VersionControl/Program.cs
+++ Task1VersionControl/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from GitHub editor!");
+            Console.WriteLine("Hello from Max's editor!");
         }
     }
 }
