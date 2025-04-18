ecf988d0ac57a80261e8f6dbf2724080b3800a19
MaxNoragami <maxcraftmd@gmail.com>
2025-04-18 16:43:13 +0300

    feat: add an extension method to the Department type

---
+++ Task8DelegatesLINQ/DepartmentExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Task8DelegatesLINQ;
+
+public static class DepartmentExtensions
+{
+    public static void GetReport(this Department department, Action<string, List<Employee>> reportMethod)
+        => reportMethod(department.Name, department.Employees);
+}
