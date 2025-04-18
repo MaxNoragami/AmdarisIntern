f3d2c2b0edf56be92873c052d765eac88305523e
MaxNoragami <maxcraftmd@gmail.com>
2025-04-18 16:40:01 +0300

    feat: add some classes based on which the collections will be created

---
+++ Task8DelegatesLINQ/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace Task8DelegatesLINQ;
+
+public class Department(string name, List<Employee> employees)
+{
+    public string Name => name;
+    public List<Employee> Employees { get; set; } = employees;
+
+    public void SalaryChange(Transformer transform)
+        => Employees.ToList().ForEach(e => e.Salary = transform(e.Salary));
+}
+
+public delegate decimal Transformer(decimal val);
---
+++ Task8DelegatesLINQ/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Task8DelegatesLINQ;
+
+public class Employee(string name, string surname, string position, decimal salary)
+{
+    public string Name => name;
+    public string Surname => surname;
+    public string Position { get; set; } = position;
+    public decimal Salary { get; set; } = salary;
+
+    public override string ToString()
+        => $"{Name} {Surname} ({Position}) : ${Salary:F2}";
+}
