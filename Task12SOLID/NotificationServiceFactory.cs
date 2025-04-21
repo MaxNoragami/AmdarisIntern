279b4c67efdb540df40e7c776d74701d2fbbddab
MaxNoragami <maxcraftmd@gmail.com>
2025-04-21 19:32:58 +0300

    feat: implement the factory method for creating the instances of notification services

---
+++ Task12SOLID/NotificationServiceFactory.cs
@@ -0,0 +1,15 @@
+﻿using Task12SOLID.NotificationServices;
+
+namespace Task12SOLID;
+
+public static class NotificationServiceFactory
+{
+    public static INotificationService Create(NotificationServiceType notificationServiceType) =>
+        notificationServiceType switch
+        {
+            NotificationServiceType.EmailNotify => new EmailNotifyService(),
+            NotificationServiceType.SMSNotify => new SMSNotifyService(),
+            NotificationServiceType.ToastNotify => new ToastNotifyService(),
+            _ => throw new NotSupportedException($"Invalid Notification Service {notificationServiceType}")
+        };
+}
