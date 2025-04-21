5d938a0169ecc5a1882e89faeafe2b6e5be0bd68
MaxNoragami <maxcraftmd@gmail.com>
2025-04-21 19:34:07 +0300

    feat: add the Main() for testing the solution

--- Task12SOLID/Program.cs
+++ Task12SOLID/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Task12SOLID;
+using Task12SOLID.Entities;
+using Task12SOLID.NotificationServices;
+using Task12SOLID.OtherServices;
+
+EnvReader.Load(".env");
+
+var local = new User("Maxim", "Alexei", "yolo@gmail.com", "061234567");
+
+INotificationService toastNotificationService = NotificationServiceFactory.Create(NotificationServiceType.ToastNotify);
+INotificationService emailNotificationService = NotificationServiceFactory.Create(NotificationServiceType.EmailNotify);
+INotificationService smsNotificationService = NotificationServiceFactory.Create(NotificationServiceType.SMSNotify);
+
+toastNotificationService.SendNotification(local, $"{local.Name}, your package has been delivered!");
+emailNotificationService.SendNotification(local, $"{local.Name}, your package has been delivered!");
+smsNotificationService.SendNotification(local, $"{local.Name}, your package has been delivered!");
