559f81e060bf7bc02ad3c9d47f70cc6eb8aa676a
MaxNoragami <maxcraftmd@gmail.com>
2025-04-22 13:46:34 +0300

    fix: change some of the properties to 'required' and for the optional ones add the [Optional] attribute in the specialized EventArgs implementation

--- Task11FileSystemStreams/BusinessOperationEventArgs.cs
+++ Task11FileSystemStreams/BusinessOperationEventArgs.cs
@@ -1,15 +1,27 @@
-﻿namespace Task11FileSystemStreams;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 
-public class BusinessOperationEventArgs(string methodName, 
-                                        bool success, 
-                                        string message = "", 
-                                        Dictionary<string, object>? parameters = null, 
-                                        Exception? exception = null) 
-                                    : EventArgs
+namespace Task11FileSystemStreams;
+
+public class BusinessOperationEventArgs: EventArgs
 {
-    public string MethodName => methodName;
-    public bool Success => success;
-    public string Message => message;
-    public Dictionary<string, object>? Parameters => parameters;
-    public Exception? Ex => exception;
+    public required string MethodName { get; init; }
+    public required bool Success { get; init; }
+    public string Message { get; init; }
+    public Dictionary<string, object>? Parameters { get; init; }
+    public Exception? Ex { get; init; }
+
+    [SetsRequiredMembers]
+    public BusinessOperationEventArgs( string methodName, 
+                                      bool success, 
+                                      [Optional] string message, 
+                                      [Optional] Dictionary<string, object>? parameters, 
+                                      [Optional] Exception? exception)
+    {
+        MethodName = methodName;
+        Success = success;
+        Message = message;
+        Parameters = parameters;
+        Ex = exception;
+    }
 }
