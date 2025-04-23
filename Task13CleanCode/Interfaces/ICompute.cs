efffb068df52c88be40e62f3e5e11ab59c8d15a9
MaxNoragami <maxcraftmd@gmail.com>
2025-04-23 16:41:19 +0300

    fix: create files for the service that computes the Registration Fee

---
+++ Task13CleanCode/Interfaces/ICompute.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+using Task13CleanCode.Entities;
+
+namespace Task13CleanCode.Interfaces
+{
+    public interface ICompute<T> where T : INumber<T>
+    {
+        public T Compute(T value);
+    }
+}
---
+++ Task13CleanCode/Services/RegistrationFeeEvaluator.cs
@@ -0,0 +1,25 @@
+﻿using Task13CleanCode.Interfaces;
+
+namespace Task13CleanCode.Services
+{
+    public class RegistrationFeeEvaluator : ICompute<int>
+    {
+        public int Compute(int value)
+        {
+            int registrationFee;
+
+            if (value > 9)
+                registrationFee = 0;
+            else if (value > 5)
+                registrationFee = 50;
+            else if (value > 3)
+                registrationFee = 100;
+            else if (value > 1)
+                registrationFee = 250;
+            else
+                registrationFee = 500;
+
+            return registrationFee;
+        }
+    }
+}
