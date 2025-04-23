e828962b3c906654ea0d1e74448d1ca5d9564fb1
MaxNoragami <maxcraftmd@gmail.com>
2025-04-23 12:41:57 +0300

    feat: added an in-memory implementation of the 'IRepository'

---
+++ Task13CleanCode/Repositories/ListRepository.cs
@@ -0,0 +1,15 @@
+﻿using BusinessLayer;
+using Task13CleanCode.Interfaces;
+
+namespace Task13CleanCode.Repositories;
+
+public class ListRepository<T>() : IRepository
+{
+    private readonly List<Speaker> _speakers = new List<Speaker>();
+
+    public int SaveSpeaker(Speaker speaker)
+    {
+        _speakers.Add(speaker);
+        return _speakers.IndexOf(speaker);
+    }
+}
